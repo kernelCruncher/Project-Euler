bcc588d987987d3c7b385c4bc70ba462863ffc88
kernelCruncher <43421475+kernelCruncher@users.noreply.github.com>
2021-06-23 20:25:25 +0100

    Create Project Euler 26.py

---
+++ Project Euler 26.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 25 11:20:44 2019
+
+"""
+
+def recursivefraction():
+    d = 1;
+    maxSum = 1;
+    while d <100:
+        for i
+        d +=1;
+    
